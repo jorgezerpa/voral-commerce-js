05561057cefb6bf0fa11f26b1aa9c10ef629db30
jorgezerpa <jorgezerpacoder@gmail.com>
2022-11-02 11:33:15 -0400

    create productCard component

---
+++ src/components/ProductCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { AddToCartButton } from './AddToCartButton'
+
+export const ProductCard = ({ item }) => {
+  return (
+    <div className=" mb-10 relative w-[230px] h-[300px] p-1 shadow-sm flex flex-col" key={item.id}>
+        <div className='rounded-lg w-[230px] h-[200px] bg-center bg-no-repeat bg-cover' style={{ backgroundImage:`url("${item.image.url}")` }}></div>
+        <h4 className="text-xl font-semibold">{item.name}</h4>
+        <p className="text-xs mb-2">{item.price.formatted_with_symbol}</p>
+        <AddToCartButton productId={item.id} />
+    </div>
+  )
+}
--- src/components/productsGrid.js
+++ src/components/productsGrid.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import { AddToCartButton } from './AddToCartButton'
 import { getProducts } from 'services/commerce'
+import { ProductCard } from './ProductCard'
 import { useMainContext } from 'Context/mainContext'
 
 export const ProductsGrid = ({  }) => {
@@ -16,14 +17,7 @@ export const ProductsGrid = ({  }) => {
 
     return (
         <div className='w-full flex gap-10 flex-wrap justify-evenly p-10'>
-            { products?.map(item=>(
-                <div className=" mb-10 relative w-[230px] h-[300px] p-1 shadow-sm flex flex-col" key={item.id}>
-                    <div className='rounded-lg w-[230px] h-[200px] bg-center bg-no-repeat bg-cover' style={{ backgroundImage:`url("${item.image.url}")` }}></div>
-                    <h4 className="text-xl font-semibold">{item.name}</h4>
-                    <p className="text-xs mb-2">{item.price.formatted_with_symbol}</p>
-                    <AddToCartButton productId={item.id} />
-                </div>
-            )) }
+            { products?.map(item=><ProductCard key={item.id} item={item}/>) }
         </div>
     )
 }
