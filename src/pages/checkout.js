0e1fb5b7fd445623af14ad84b5a0a835f63ca7f2
jorgezerpa <jorgezerpacoder@gmail.com>
2022-11-04 10:45:17 -0400

    incorporate paypal payment (independant of commerce js

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -6,47 +6,28 @@ import { CheckoutForm } from '@components/checkoutForm'
 import { useForm } from 'hooks/useForm'
 
 export const Checkout = () => {
-    const { formRef, handleSubmit, data, wasSubmitted } = useForm()
-    const { cart, setCheckoutToken, checkoutToken, setCheckout, checkout } = useMainContext()
-    const [payerId, setPayerId] = useState('')
-    const [paymentId, setPaymentId] = useState('')
-    const [recipe, setRecipe] = useState({})
-    const [approved, setApproved] = useState(false)
-    
-    useEffect(()=>{createCheckout(cart?.id).then(data=>setCheckoutToken(data))}, [])
-    
-    useEffect(()=>{
-        if(wasSubmitted){
-            captureOrder(checkoutToken.id, data).then(data => setPaymentId(data.payment_id))
-        }
-    }, [wasSubmitted])
-
-    useEffect(()=>{
-        if(approved){
-            executeOrder(checkoutToken.id, data, paymentId, payerId)
-                .then(data => setRecipe(data))
-        }
-    }, [approved])
+  const { formRef, handleSubmit, data, wasSubmitted } = useForm()
+  const { cart, setCheckoutToken, checkoutToken, setCheckout, checkout } = useMainContext()
+  const [paypalOrder, setPaypalOrder] = useState({})
+  console.log(paypalOrder)
 
     const createOrder = (data, actions) => {
         return actions.order.create({
             currency_code: "USD",   
             purchase_units: [{amount:{value:cart.subtotal.raw, currency_code:"USD" }}],
-            // application_context:{return_url:'http://localhost:3000/wujuu', cancel_url:'http://localhost:3000/naaa'}
         }).then(orderId=> orderId)
     }
 
     const onApprove = async(data, actions) => {
-            setPayerId(data.payerID)
-            setApproved(true)
+        console.log('aqui toy')  
+        actions.order.capture().then((data)=>{setPaypalOrder(data)})
     } 
-
     return (
     <>
     <div className='py-10'>
         <CheckoutForm formRef={formRef} handleSubmit={handleSubmit}/>
         <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID }} >
-            <PayPalButtons createOrder={createOrder} onApprove={onApprove}  ></PayPalButtons>
+            <PayPalButtons createOrder={createOrder} onApprove={onApprove}></PayPalButtons>
         </PayPalScriptProvider>
     </div>
     </>
