865e448157809c6be328ba79ad9c86b3953df199
jorgezerpa <jorgezerpacoder@gmail.com>
2022-11-11 14:11:33 -0400

    solve mobile sidebar bug (toggle button was not working)

--- src/components/SidebarMenu.js
+++ src/components/SidebarMenu.js
@@ -42,7 +42,7 @@ export const SidebarMenu = () => {
     }
 
     return (
-        <div className={`bg-white z-50 ${windowWidth > 500 ? "w-[120px]":"w-[200px]"} py-[10px] transition ease-in-out ${windowWidth>500 ? "relative":`absolute translate-x-[${!showSideBar && "-200px"}]`}`}>
+        <div className={`bg-white z-50 ${windowWidth > 500 ? "w-[120px]":"w-[200px]"} py-[10px] transition ease-in-out ${windowWidth>500 ? "relative":`absolute ${showSideBar && "translate-x-[-200px]"}`}`}>
             <div className={`p-2 rounded-[50%] absolute top-5 left-full  bg-pink-300 ${windowWidth>500 && "hidden"}`} onClick={toggleShowSideBar}>
                 <BiCategoryAlt size={35} color="#fff" />
             </div>
